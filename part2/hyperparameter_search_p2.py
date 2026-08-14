a2ccab82633608d37024b3a0dd874b9b5a8a2caa
Vladimir P <dilecrox@gmail.com>
2026-08-14 15:47:34 -0400

    added shap plots and changed tuning

--- part2/hyperparameter_search_p2.py
+++ part2/hyperparameter_search_p2.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.pipeline import Pipeline
@@ -15,20 +16,28 @@ CSV_DIR = BASE_DIR/'csv'
 
 morgan = pd.read_csv(CSV_DIR/'qm9_fingerprints_p2(2048).csv')
 
+# convert to float32 — halves memory vs default float64
+bit_cols = [c for c in morgan.columns if c.startswith('bit_')]
+morgan[bit_cols] = morgan[bit_cols].astype('float32')
+
+# get cuda device
+xgb.collective.init()
+local_rank = xgb.collective.get_rank()
+
 # variables
 random_state = 71
-device = 'cuda'
+device = 'cpu'
 tree_method = 'hist'
-n_jobs = -1
 objective = 'reg:squarederror'
+n_jobs = -1
 
 # parameter grids
 xgb_param = {
-    'regressor__n_estimators': [50, 100, 200, 300],
-    'regressor__max_depth': [2, 4, 8],
-    'regressor__learning_rate': [0.01, 0.05, 0.1, 0.2],
-    'regressor__subsample': [0.5, 0.7, 1.0],
-    'regressor__colsample_bytree': [0.5, 0.7, 1.0]
+    'regressor__n_estimators': [50, 100, 200, 300, 500],
+    'regressor__max_depth': [8, 12, 16],
+    'regressor__learning_rate': [0.01, 0.05, 0.1, 0.2, 0.5],
+    'regressor__subsample': [0.7, 1.0, 1.5, 2.0],
+    'regressor__colsample_bytree': [0.7, 1.0, 1.5, 2.0]
 }
 
 # data storage
@@ -46,53 +55,60 @@ m_train, m_test, mt_train, mt_test = train_test_split(
     random_state=random_state
 )
 
-# morgan pipelines
+# ---Morgan Pipelines---
+# XGBoost
 morg_XGB = Pipeline(steps=[
     ('scaler', StandardScaler()),
     ('regressor', xgb.XGBRegressor(
         device=device,
         tree_method=tree_method,
         random_state=random_state,
-        objective=objective
+        objective=objective,
+        n_jobs=n_jobs
     ))
 ])
+morg_XGB_search = RandomizedSearchCV(
+    morg_XGB,
+    param_distributions=xgb_param,
+    n_iter=100,
+    cv=5,
+    n_jobs=2,
+    scoring='neg_mean_absolute_error',
+    random_state=random_state,
+    verbose=1
+)
+
+morg_XGB_search.fit(m_train, mt_train)
+Morg_XGB_best_params = morg_XGB_search.best_params_
+del morg_XGB, morg_XGB_search
+gc.collect()
+
+# Random Forest
 morg_RF = Pipeline(steps=[
     ('scaler', StandardScaler()),
     ('regressor', xgb.XGBRFRegressor(
         device=device,
         tree_method=tree_method,
         random_state=random_state,
-        objective=objective
+        objective=objective,
+        n_jobs=n_jobs
     ))
 ])
-
-morg_XGB_search = RandomizedSearchCV(
-    morg_XGB,
-    param_distributions=xgb_param,
-    n_iter=100,
-    cv=5,
-    n_jobs=n_jobs,
-    scoring='neg_mean_absolute_error',
-    random_state=random_state,
-    verbose=2
-)
 morg_RF_search = RandomizedSearchCV(
     morg_RF,
     param_distributions=xgb_param,
     n_iter=100,
     cv=5,
-    n_jobs=n_jobs,
+    n_jobs=2,
     scoring='neg_mean_absolute_error',
     random_state=random_state,
-    verbose=2
+    verbose=1
 )
 
-morg_XGB_search.fit(m_train, mt_train)
 morg_RF_search.fit(m_train, mt_train)
-Morg_XGB_best_params = morg_XGB_search.best_params_
 Morg_RF_best_params = morg_RF_search.best_params_
 
-del m_train, m_test, mt_train, mt_test, mdata, mtarget
+del m_train, m_test, mt_train, mt_test, mdata, morg_RF, morg_RF_search
 gc.collect()
 
 # descriptor data
@@ -108,54 +124,62 @@ d_train, d_test, dt_train, dt_test = train_test_split(
 )
 
 # descriptor pipelines
-disc_XGB = Pipeline(steps=[
+desc_XGB = Pipeline(steps=[
     ('scaler', StandardScaler()),
     ('regressor', xgb.XGBRegressor(
         device=device,
         tree_method=tree_method,
         random_state=random_state,
-        objective=objective
+        objective=objective,
+        n_jobs=n_jobs
     ))
 ])
-disc_RF = Pipeline(steps=[
+desc_RF = Pipeline(steps=[
     ('scaler', StandardScaler()),
     ('regressor', xgb.XGBRFRegressor(
         device=device,
         tree_method=tree_method,
         random_state=random_state,
-        objective=objective
+        objective=objective,
+        n_jobs=n_jobs
     ))
 ])
 
-disc_XGB_search = RandomizedSearchCV(
-    disc_XGB,
+desc_XGB_search = RandomizedSearchCV(
+    desc_XGB,
     param_distributions=xgb_param,
     n_iter=100,
     cv=5,
-    n_jobs=n_jobs,
+    n_jobs=-1,
     scoring='neg_mean_absolute_error',
     random_state=random_state,
-    verbose=2
+    verbose=1
 )
-disc_RF_search = RandomizedSearchCV(
-    disc_RF,
+desc_RF_search = RandomizedSearchCV(
+    desc_RF,
     param_distributions=xgb_param,
     n_iter=100,
     cv=5,
-    n_jobs=n_jobs,
+    n_jobs=-1,
     scoring='neg_mean_absolute_error',
     random_state=random_state,
-    verbose=2
+    verbose=1
 )
 
-disc_XGB_search.fit(d_train, dt_train)
-disc_RF_search.fit(d_train, dt_train)
+desc_XGB_search.fit(d_train, dt_train)
+desc_RF_search.fit(d_train, dt_train)
 
+print(f'Morg XGB: {Morg_XGB_best_params}')
+print(f'Morg RF: {Morg_RF_best_params}')
+
+print(f'Desc XGB: {desc_XGB_search.best_params_}')
+print(f'desc RF: {desc_RF_search.best_params_}')
+exit()
 # ---Descriptors XGBoost SHAP analysis---
 # shap values
-XGB_preprocessor = disc_XGB_search.best_estimator_.named_steps['scaler']
+XGB_preprocessor = desc_XGB_search.best_estimator_.named_steps['scaler']
 d_train_scaled_XGB = XGB_preprocessor.transform(d_train)
-xgb_model = disc_XGB_search.best_estimator_.named_steps['regressor']
+xgb_model = desc_XGB_search.best_estimator_.named_steps['regressor']
 
 XGB_explainer = shap.TreeExplainer(xgb_model)
 XGB_shap_values = XGB_explainer.shap_values(d_train_scaled_XGB)
@@ -191,9 +215,9 @@ plt.show()
 
 # ---Descriptors Random Forest SHAP analysis---
 # shap values
-RF_preprocessor = disc_RF_search.best_estimator_.named_steps['scaler']
+RF_preprocessor = desc_RF_search.best_estimator_.named_steps['scaler']
 d_train_scaled_RF = RF_preprocessor.transform(d_train)
-rf_model = disc_RF_search.best_estimator_.named_steps['regressor']
+rf_model = desc_RF_search.best_estimator_.named_steps['regressor']
 
 RF_explainer = shap.TreeExplainer(rf_model)
 RF_shap_values = RF_explainer.shap_values(d_train_scaled_RF)
@@ -234,8 +258,8 @@ def strip_prefix(params, prefix='regressor__'):
 best_params = {
     'morgan_xgb': strip_prefix(Morg_XGB_best_params),
     'morgan_rf': strip_prefix(Morg_RF_best_params),
-    'desc_xgb': strip_prefix(disc_XGB_search.best_params_),
-    'desc_rf': strip_prefix(disc_RF_search.best_params_)
+    'desc_xgb': strip_prefix(desc_XGB_search.best_params_),
+    'desc_rf': strip_prefix(desc_RF_search.best_params_)
 }
 
 with open(BASE_DIR / 'best_params_p2.json', 'w') as f:
