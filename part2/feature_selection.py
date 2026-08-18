32004b5fb5dd19c36203ac11753728071af0c6f6
Vladimir P <dilecrox@gmail.com>
2026-08-18 15:34:35 -0400

    added SHAP graphs and RF SHAP

--- part2/feature_selection.py
+++ part2/feature_selection.py
@@ -1,13 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
-from pathlib import Path
-from sklearn.model_selection import train_test_split
+import shap
 from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 import xgboost as xgb
-import shap
-import numpy as np
 import json
+from pathlib import Path
 
 
 BASE_DIR = Path(__file__).parent
@@ -36,6 +37,8 @@ d_train, d_test, dt_train, dt_test = train_test_split(
 with open(BASE_DIR / 'best_params_p2.json', 'r') as f:
     best_params = json.load(f)
 
+#   ---XGBoost---
+# pipeline
 desc_xgb = Pipeline(steps=[
     ('scaler', StandardScaler()),
     ('regressor', xgb.XGBRegressor(       
@@ -50,6 +53,7 @@ desc_xgb = Pipeline(steps=[
 
 desc_xgb.fit(d_train, dt_train)
 
+# shap values
 XGB_preprocessor = desc_xgb.named_steps['scaler']
 d_train_scaled_XGB = XGB_preprocessor.transform(d_train)
 xgb_model = desc_xgb.named_steps['regressor']
@@ -57,6 +61,36 @@ xgb_model = desc_xgb.named_steps['regressor']
 XGB_explainer = shap.TreeExplainer(xgb_model)
 XGB_shap_values = XGB_explainer.shap_values(d_train_scaled_XGB)
 
+# --Plot--
+# bar plot
+plt.figure()
+shap.summary_plot(XGB_shap_values, 
+                  d_train_scaled_XGB, 
+                  feature_names=d_train.columns.tolist(), 
+                  plot_type='bar', 
+                  show=False
+                )
+
+plt.title('XGBoost Feature Importance (SHAP)')
+plt.tight_layout()
+plt.savefig(BASE_DIR/'images/xgb_shap_bar.png', dpi=150, bbox_inches='tight')
+plt.show()
+
+# beeswarm plot
+plt.figure()
+shap.summary_plot(XGB_shap_values, 
+                  d_train_scaled_XGB, 
+                  feature_names=d_train.columns.tolist(), 
+                  plot_type='dot',
+                  show=False
+                )
+
+plt.title('XGBoost SHAP Beeswarm')
+plt.tight_layout()
+plt.savefig(BASE_DIR/'images/xgb_shap_beeswarm.png', dpi=150, bbox_inches='tight')
+plt.show()
+
+# --Feature importance--
 # identify low SHAP features
 feature_names = d_train.columns.tolist()
 mean_shap = np.abs(XGB_shap_values).mean(axis=0)
@@ -80,7 +114,7 @@ d_test_reduced = d_test.drop(columns=low_shap)
 disc_XGB_reduced = Pipeline(steps=[
     ('scaler', StandardScaler()),
     ('regressor', xgb.XGBRegressor(
-        **best_params['desc_rf'],
+        **best_params['desc_xgb'],
         device=device,
         tree_method=tree_method,
         random_state=random_state,
@@ -96,3 +130,99 @@ reduced_mae = mean_absolute_error(dt_test, disc_XGB_reduced.predict(d_test_reduc
 print(f'Full features MAE: {full_mae:.4f}')
 print(f'Reduced features MAE: {reduced_mae:.4f}')
 print(f'Difference: {abs(full_mae - reduced_mae):.4f}')
+
+#   ---Random Forests---
+desc_rf = Pipeline(steps=[
+    ('scaler', StandardScaler()),
+    ('regressor', xgb.XGBRFRegressor(       
+        **best_params['desc_rf'],
+        device=device,
+        tree_method=tree_method,
+        random_state=random_state,
+        objective=objective,
+        n_jobs=n_jobs))
+    ]
+)
+
+desc_rf.fit(d_train, dt_train)
+
+# shap values
+RF_preprocessor = desc_rf.named_steps['scaler']
+d_train_scaled_RF = RF_preprocessor.transform(d_train)
+rf_model = desc_rf.named_steps['regressor']
+
+RF_explainer = shap.TreeExplainer(rf_model)
+RF_shap_values = RF_explainer.shap_values(d_train_scaled_RF)
+
+# --Plot--
+# bar plot
+plt.figure()
+shap.summary_plot(RF_shap_values, 
+                  d_train_scaled_RF, 
+                  feature_names=d_train.columns.tolist(), 
+                  plot_type='bar', 
+                  show=False
+                )
+
+plt.title('Random Forests Feature Importance (SHAP)')
+plt.tight_layout()
+plt.savefig(BASE_DIR/'images/rf_shap_bar.png', dpi=150, bbox_inches='tight')
+plt.show()
+
+# beeswarm plot
+plt.figure()
+shap.summary_plot(RF_shap_values, 
+                  d_train_scaled_RF, 
+                  feature_names=d_train.columns.tolist(), 
+                  plot_type='dot',
+                  show=False
+                )
+
+plt.title('Random Forests SHAP Beeswarm')
+plt.tight_layout()
+plt.savefig(BASE_DIR/'images/rf_shap_beeswarm.png', dpi=150, bbox_inches='tight')
+plt.show()
+
+# identify low SHAP features
+feature_names = d_train.columns.tolist()
+mean_shap = np.abs(RF_shap_values).mean(axis=0)
+shap_df = pd.DataFrame({
+    'feature': feature_names,
+    'mean_shap': mean_shap
+}).sort_values('mean_shap', ascending=False)
+print(shap_df)
+
+# identify features below threshold
+low_shap = shap_df[shap_df['mean_shap'] < 0.0005]['feature'].tolist()
+print(f'Low SHAP features: {low_shap}')
+
+full_mae = mean_absolute_error(dt_test, 
+    desc_rf.predict(d_test))
+
+# reduced feature MAE  
+d_train_reduced = d_train.drop(columns=low_shap)
+d_test_reduced = d_test.drop(columns=low_shap)
+
+disc_RF_reduced = Pipeline(steps=[
+    ('scaler', StandardScaler()),
+    ('regressor', xgb.XGBRFRegressor(
+        **best_params['desc_rf'],
+        device=device,
+        tree_method=tree_method,
+        random_state=random_state,
+        objective=objective,
+        n_jobs=n_jobs)
+        )
+    ]
+)
+
+disc_RF_reduced.fit(d_train_reduced, dt_train)
+reduced_mae = mean_absolute_error(dt_test, disc_RF_reduced.predict(d_test_reduced))
+
+print(f'Full features MAE: {full_mae:.4f}')
+print(f'Reduced features MAE: {reduced_mae:.4f}')
+print(f'Difference: {abs(full_mae - reduced_mae):.4f}')
+
+
+
+
